553e4ca6346389eb25dc932f30812622383edb8a
youssef <youssef.charroud445@gmail.com>
2025-10-10 18:00:38 +0100

    save

---
+++ src/page/CreateProgramPage.jsx
@@ -0,0 +1,16 @@
+import { Card, CardHeader } from '@/components/ui/card'
+import React from 'react'
+
+const CreateProgramPage = () => {
+  return (
+    <div>
+      <Card>
+        <CardHeader>
+          <h1 className='heading capitalize'> Add New Program </h1>
+        </CardHeader>
+      </Card>
+    </div>
+  )
+}
+
+export default CreateProgramPage
--- src/router.jsx
+++ src/router.jsx
@@ -1,10 +1,30 @@
 import { createBrowserRouter } from "react-router";
 import Home from "./page/Home";
+import CreateProgramPage from "./page/CreateProgramPage";
+import MainLayout from "./layout/MainLayout";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    element: <MainLayout />,
+    children: [
+      {
+        path: "coach",
+        children: [
+          {
+            path: "program/create",
+            element: <CreateProgramPage />,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <div>404 not found</div>,
   },
 ]);
 
