bd9dcef7926967c2dc3df431520e6dbcfe9c602b
youssef <youssef.charroud445@gmail.com>
2025-10-13 17:19:21 +0100

    feature:add css variables for shadcn

--- src/page/CreateProgramPage.jsx
+++ src/page/CreateProgramPage.jsx
@@ -1,16 +1,16 @@
-import { Card, CardHeader } from '@/components/ui/card'
-import React from 'react'
+import { Card, CardHeader } from "@/components/ui/card";
+import React from "react";
 
 const CreateProgramPage = () => {
   return (
     <div>
-      <Card>
+      <Card className={"shadow-md"}>
         <CardHeader>
-          <h1 className='heading capitalize'> Add New Program </h1>
+          <h1 className="heading capitalize"> Add New Program </h1>
         </CardHeader>
       </Card>
     </div>
-  )
-}
+  );
+};
 
-export default CreateProgramPage
+export default CreateProgramPage;
