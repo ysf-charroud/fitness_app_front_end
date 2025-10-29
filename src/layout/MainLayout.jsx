bc735eee07b79358702768f6771901496d26bc21
Youssef-cha <youssef.charroud445@gmail.com>
2025-10-30 00:47:11 +0100

    add auth slice to redux store - setup interceptors for axios to apply auto refresh and bind the refresh token

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,14 +1,19 @@
 import CoachSideBar from "@/components/CoachSideBar";
-import { Outlet } from "react-router";
+import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router";
 
 const CoachLayout = () => {
-  return (
-    <main className="bg-background min-h-screen">
-      <CoachSideBar>
-        <Outlet />
-      </CoachSideBar>
-    </main>
-  );
+  const user = useSelector((state) => state.auth.user);
+  if (user.role !== "admin") return <Navigate to="/login" replace />;
+  if (user.role !== "athlete") return <Navigate to="/login" replace />;
+  if (user.role !== "coach")
+    return (
+      <main className="bg-background min-h-screen">
+        <CoachSideBar>
+          <Outlet />
+        </CoachSideBar>
+      </main>
+    );
 };
 
 export default CoachLayout;
